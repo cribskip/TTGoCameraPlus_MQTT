391f2030d465d0653c47b87661869fa4427b1023
cribskip <sascha.kloss@gmx.de>
2019-06-16 09:17:56 +0200

    Update ESP_DoorCam.ino

--- ESP_DoorCam.ino
+++ ESP_DoorCam.ino
@@ -1,20 +1,24 @@
 // TTGO Camera Plus (OV2640 + TFT ST7789)
-// Compile as Board AI Thinker ESP32-CAM
+// Compile as Board ESP32 Dev Module
+// CPU MHz: 240 MHz
+// Flash Frequency: 40 MHz
+// Flash Mode: DIO
+// Flash size: 4MB (32Mb)
+// Partition: Huge App
+// PSRAM: Enabled
+
+// Used libraries:
+// TFT_eSPI
+// JPEG_Decoder
+
+// Current Performance
+// HQVGA, JPEG Decode ~68ms, Total Render time per Frame ~86ms = 11,6 frames/second
 
 #include "esp_camera.h"
 #include <WiFi.h>
 
-#include <Adafruit_GFX.h>    // Core graphics library
-#include <Adafruit_ST7789.h> // Hardware-specific library for ST7789
-#include <SPI.h>
-#define TFT_CS        12
-#define TFT_DC        15
-#define TFT_BACKLIGHT  2
-Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, 19, 21, -1);
-//
-// WARNING!!! Make sure that you have either selected ESP32 Wrover Module,
-//            or another board which has PSRAM enabled
-//
+#include <TFT_eSPI.h>       // Hardware-specific library
+TFT_eSPI tft = TFT_eSPI();  // Invoke custom library
 
 // Select camera model
 #define CAMERA_MODEL_TTGO
@@ -26,13 +30,14 @@ const char* ssid = "";
 const char* password = "";
 
 void startCameraServer();
+camera_fb_t * fb = NULL;
 
-void setup() {
-  Serial.begin(115200);
-  Serial.setDebugOutput(true);
-  Serial.println();
-  Serial.println(F("BOOT"));
-  WiFi.mode(WIFI_OFF);    //This also works
+void config_camera() {
+  if (! psramFound()) {
+    while (1) {
+      Serial.println(F("YOUR BOARD DOES NOT HAVE PSRAM!"));
+    }
+  }
 
   camera_config_t config;
   config.ledc_channel = LEDC_CHANNEL_0;
@@ -55,45 +60,25 @@ void setup() {
   config.pin_reset = RESET_GPIO_NUM;
   config.xclk_freq_hz = 20000000;
   config.pixel_format = PIXFORMAT_JPEG;
-  //init with high specs to pre-allocate larger buffers
-  if (psramFound()) {
-    // FRAMESIZE_UXGA
-    config.frame_size = FRAMESIZE_QVGA;
-    config.jpeg_quality = 10;
-    config.fb_count = 2;
-  }
+  //PIXFORMAT_YUV422;  // Colors incorrect, Picture ok
+  //PIXFORMAT_JPEG; // Has to be decoded most of the time
+  //PIXFORMAT_RGB565; // Colors incorrect, Picture ok
+  //PIXFORMAT_GRAYSCALE; // Colors incorrect, Picture ok
+
+  //FRAMESIZE_UXGA
+  //FRAMESIZE_HQVGA; // 240x160
+  //FRAMESIZE_QVGA;  // 320x240
+  config.frame_size = FRAMESIZE_HQVGA;
+  config.jpeg_quality = 10;
+  config.fb_count = 2;
 
   // camera init
   esp_err_t err = esp_camera_init(&config);
   if (err != ESP_OK) {
-    Serial.printf("Camera init failed with error 0x%x", err);
-    return;
+    while (1) {
+      Serial.println(F("Camera init failed"));
+    }
   }
-
-  pinMode(TFT_BACKLIGHT, OUTPUT);
-  digitalWrite(TFT_BACKLIGHT, LOW); // Backlight on
-
-  tft.init(240, 240);           // Init ST7789 240x240
-  tft.setFont();
-  tft.setRotation(2);
-  tft.fillScreen(0x0000);
-  tft.setTextColor(0xFFFF);
-  tft.setTextSize(2);
-
-
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(250);
-    Serial.print(".");
-  }
-  tft.print(F("WiFi"));
-
-  startCameraServer();
-  tft.print(F("WWW"));
-  digitalWrite(TFT_BACKLIGHT, HIGH); // Backlight on
-
-  Serial.print(F("Camera Ready! Use 'http://"));
-  Serial.print(WiFi.localIP());
 
   // set window / digital zoom
   //s->set_window_size(s, 1600 / 8 - resolution[CAM_FRAMESIZE][0] / 2, 1200 / 8 - resolution[CAM_FRAMESIZE][1] / 2, resolution[CAM_FRAMESIZE][0], resolution[CAM_FRAMESIZE][1]);
@@ -101,12 +86,62 @@ void setup() {
   sensor_t * s = esp_camera_sensor_get();
   s->set_hmirror(s, true);
   s->set_vflip(s, true);
-  s->set_gainceiling(s, (gainceiling_t)128);
+  s->set_gainceiling(s, GAINCEILING_128X);
   //s->set_framesize(s, (framesize_t)7);
+}
+
+void setup() {
+  WiFi.mode(WIFI_OFF);
+  pinMode(TFT_BACKLIGHT, OUTPUT);
+  digitalWrite(TFT_BACKLIGHT, LOW); // Backlight off
+
+  Serial.begin(115200);
+  Serial.setDebugOutput(true);
+  Serial.println();
+  Serial.println(F("BOOT"));
+  config_camera();
+
+  tft.begin();
+  //tft.setFont();
+  //tft.setRotation(2);
+  tft.fillScreen(TFT_DARKGREY);
+  //tft.setTextColor(0xFFFF);
+  //tft.setTextSize(2);
+
+  WiFi.begin(ssid, password);
+  //while (WiFi.status() != WL_CONNECTED) {
+  delay(250);
+  Serial.print(".");
+  //}
+  Serial.print(F("WiFi"));
+
+  startCameraServer();
+  Serial.print(F("WWW"));
+  digitalWrite(TFT_BACKLIGHT, HIGH); // Backlight on
+  delay(1000);
+  
+  Serial.print(F("Camera Ready! Use 'http://"));
+  //Serial.print(WiFi.localIP());
+}
+
+void localstream() {
+  fb = esp_camera_fb_get();
+  esp_err_t res = ESP_OK;
+  
+  if (!fb) {
+    Serial.println(F("Camera capture failed"));
+  }
+
+  if (fb->format == PIXFORMAT_JPEG) {
+    drawArrayJpeg(fb->buf, fb->len, 0, 0);
+  }
+  esp_camera_fb_return(fb);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   //delay(10000);
   yield();
+  //if (!fb) fb = esp_camera_fb_get();
+  localstream();
 }
